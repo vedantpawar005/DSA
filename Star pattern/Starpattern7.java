fad76c92accc6e3734df6468f1ff8357b608ba56
vedantpawar005 <vedantpawar005@gmail.com>
2025-08-31 14:32:50 +0530

    Update Starpattern7.java

--- Star pattern/Starpattern7.java
+++ Star pattern/Starpattern7.java
@@ -1,27 +1,23 @@
-package dsaPrep;
-
-public class Starpattern7 {
-
-	public static void main(String[] args) {
-		int a=1;
-		for(int i=5;i>0;i--) 
-		{
-			for(int space=0;space<i;space++) 
-			{
-				System.out.print(" ");
-			}
-			for(int j=1;j<=a;j++) 
-			{
-				System.out.print("*");
-			}
-			System.out.println();
-			a+=2;
-		}
-		
-
-	}
-
-}
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        int space=n-1;
+        for(int i=0;i<n;i++)
+        {
+            for(int s=space;s>0;s--)
+            {
+                cout<<" ";
+            }
+            space--;
+            for(int j=0;j<i*2+1;j++)
+            {
+                cout<<"*";
+            }
+            cout<<"\n";
+        }
+    }
+};
 /*
 
      *
